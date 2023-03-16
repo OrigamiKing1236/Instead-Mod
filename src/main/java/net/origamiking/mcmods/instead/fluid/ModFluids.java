8a31e9548f479c77d9ffa09a47c4a566e5df002c
OrigamiKing3612 <origamiking3612@gmail.com>
2023-03-16 15:07:26 -0500

    update on stuff

--- src/main/java/net/origamiking/mcmods/instead/fluid/ModFluids.java
+++ src/main/java/net/origamiking/mcmods/instead/fluid/ModFluids.java
@@ -24,8 +24,5 @@ public class ModFluids{
         FLOWING_ACID = Registry.register(Registries.FLUID, new Identifier(Insteadmain.MOD_ID, "flowing_acid"), new AcidFluid.Flowing());
         ACID_BLOCK = Registry.register(Registries.BLOCK, new Identifier(Insteadmain.MOD_ID, "acid_block"), new FluidBlock(ModFluids.STILL_ACID, FabricBlockSettings.copyOf(Blocks.LAVA)){ });
         ACID_BUCKET = Registry.register(Registries.ITEM, new Identifier(Insteadmain.MOD_ID, "acid_bucket"), new BucketItem(ModFluids.STILL_ACID, new FabricItemSettings().recipeRemainder(Items.BUCKET).maxCount(1)));
-
-
-
     }      
 }
